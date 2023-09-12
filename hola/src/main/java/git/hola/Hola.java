61b846f0f89c5661d6f965de8e85af36616d2481
luis mendez <victorcasla9@gmail.com>
2023-09-12 11:17:11 -0300

    laboratorio1 terminado

---
+++ hola/src/main/java/git/hola/Hola.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package git.hola;
+
+/**
+ * La clase hola construye un programa que visualizara el mensaje
+ * "hola, me llamo Angel" en el dispositivo de salida estandar
+ */
+public class Hola {
+
+    public static void main(String[] args) {
+        //Visualiza por pantalla "Hola, me llamo Angel"
+        System.out.println("Hola, me llamo Angel");
+    }
+}
